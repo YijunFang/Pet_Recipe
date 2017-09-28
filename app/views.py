d85a93912b783936264d953bb4eefec68df432f1
jfan <jfan9988@gmail.com>
2017-09-28 13:24:58 +1000

    added filter js, improved result db query and page formatting

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 # from Pet_Recipe.LoginForm import LoginForm
 # from main import app
 from .forms import LoginForm
-
+from sqlalchemy import or_
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 's3cr3t'
@@ -12,15 +12,8 @@ from app.models import *
 
 @app.route('/')
 def index():
-	"""
-		example code for selecting (call query in sqlalchemy)
-		
-	for item in db_session.query(Recipe.id, Recipe.ingredient,Recipe.instruction):
-		print (item)
-	"""
 	recipes = db_session.query(Recipe,Pet_type,User).filter(Recipe.type==Pet_type.id). \
 							 filter(Recipe.user_id==User.id).all()
-	print(recipes)
 
 	return render_template("home.html",recipes=recipes)  
 
@@ -28,9 +21,16 @@ def index():
 def search_view():
 	search_term = request.form['search_term']
 	pet_type = Pet_type.query.all()
-	user = User.query.all()
-	recipes = db_session.query(Recipe,Pet_type,User).filter(Recipe.type==Pet_type.id). \
-							 filter(Recipe.user_id==User.id).all()
+	user = db_session.query(User).filter(User.user_name.contains(search_term)).all()
+
+	#search for search_term in recipe title,instruction & ingredients
+	recipes = db_session.query(Recipe,Pet_type,User).\
+							 filter(Recipe.type==Pet_type.id). \
+							 filter(Recipe.user_id==User.id). \
+							 filter(or_(Recipe.title.contains(search_term), \
+							 	        Recipe.ingredient.contains(search_term), \
+							 	        Recipe.instruction.contains(search_term))).all()
+	
 	return render_template("result_page.html",search_term=search_term, pet_type=pet_type, recipes=recipes, user = user) 
 
 @app.route('/profile')
